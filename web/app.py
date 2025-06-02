0dd44435ed606d588c952c606d95f7dc56c84654
Prakhar Jain <78018165+Prakhar8003@users.noreply.github.com>
2025-06-02 18:09:21 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -23,4 +23,4 @@ def index():
     return render_template('index.html', password=password, error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
